1421b81f24644c4c5878a19905ef43e45d650e0e
JuntoNova <andrew@juntonova.com>
2026-09-05 13:49:34 -0700

    Restore app.js without document.write so iOS does not hang

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,19 @@
-document.write('<script src="https://cdn.jsdelivr.net/gh/JuntoNova/tank-rank@4d2b1729cda9b9a40965b36e5c5f5ce51b72951a/assets/app.js"><\/script>');
+(function () {
+  function load(url) {
+    var x = new XMLHttpRequest();
+    x.open("GET", url, false);
+    x.send();
+    return x.responseText;
+  }
+  var src = load("https://cdn.jsdelivr.net/gh/JuntoNova/tank-rank@4d2b1729cda9b9a40965b36e5c5f5ce51b72951a/assets/app.js");
+  src = src.replace(
+    '<div class="kicker">Archive</div>',
+    '<div class="kicker">Past</div>'
+  );
+  var future = src.match(/<a class="door" href=\"\.\/upcoming\.html\">[\s\S]*?<\/a>/);
+  var historic = src.match(/<a class="door" href=\"\.\/drafts\.html\">[\s\S]*?<\/a>/);
+  if (future && historic) {
+    src = src.replace(future[0] + "\n        " + historic[0], historic[0] + "\n        " + future[0]);
+  }
+  eval(src);
+})();
