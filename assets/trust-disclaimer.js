77a4e66b277b73415ca8cf49765783d5de8d2a96
JuntoNova <andrew@juntonova.com>
2026-09-11 15:28:57 -0700

    Hide leftover prototype banner after render

--- assets/trust-disclaimer.js
+++ assets/trust-disclaimer.js
@@ -1,3 +1,6 @@
 (function () {
   if (window.TANK_RANK) TANK_RANK.disclaimer = "";
+  var s = document.createElement("style");
+  s.textContent = ".banner{display:none !important}";
+  document.documentElement.appendChild(s);
 })();
