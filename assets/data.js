df530bb189b2a0909033d8b75aa150a85e03e313
JuntoNova <andrew@juntonova.com>
2026-09-12 08:49:30 -0700

    PR-D gate: restore data.js as part loader (unbreak PLACEHOLDER)

--- assets/data.js
+++ assets/data.js
@@ -1,1 +1,15 @@
-PLACEHOLDER
+(function () {
+  function load(url) {
+    var x = new XMLHttpRequest();
+    x.open("GET", url, false);
+    x.send();
+    return x.responseText;
+  }
+  // PR-D packs assembled from split parts (blank 2028/2029 odds + living-2027-bios load).
+  var src = "";
+  src += load("assets/data-part1.js");
+  src += load("assets/data-part2.js");
+  src += load("assets/data-part3.js");
+  src += load("assets/data-part4.js");
+  eval(src);
+})();
